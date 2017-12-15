6dd6395ae73c70b1fe34c22523ab727ac962c907
Huaitian Lu, Yikun Li <y.li.76@student.rug.nl>
2017-12-15 12:42:35 +0100

    update

---
+++ ros/catkin_ws/src/object_recognition/scripts/network.py
@@ -0,0 +1,108 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tempfile
+
+import tensorflow as tf
+
+FLAGS = None
+
+
+class Network:
+
+    def __init__(self):
+        pass
+
+    def build(self, args):
+        # Reshape to use within a convolutional neural net.
+        # Last dimension is for "features" - there is only one here, since images are
+        # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+        with tf.name_scope('reshape'):
+            x_image = tf.reshape(x, [-1, 28, 28, 3])
+
+        # First convolutional layer - maps one grayscale image to 32 feature maps.
+        with tf.name_scope('conv1'):
+            W_conv1 = self.weight_variable([5, 5, 1, 32])
+            b_conv1 = self.bias_variable([32])
+            h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
+
+        # Pooling layer - downsamples by 2X.
+        with tf.name_scope('pool1'):
+            h_pool1 = self.max_pool_2x2(h_conv1)
+
+        # Second convolutional layer -- maps 32 feature maps to 64.
+        with tf.name_scope('conv2'):
+            W_conv2 = self.weight_variable([5, 5, 32, 64])
+            b_conv2 = self.bias_variable([64])
+            h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+
+        # Second pooling layer.
+        with tf.name_scope('pool2'):
+            h_pool2 = self.max_pool_2x2(h_conv2)
+
+        # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+        # is down to 7x7x64 feature maps -- maps this to 1024 features.
+        with tf.name_scope('fc1'):
+            W_fc1 = self.weight_variable([7 * 7 * 64, 1024])
+            b_fc1 = self.bias_variable([1024])
+
+            h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+        # Dropout - controls the complexity of the model, prevents co-adaptation of
+        # features.
+        with tf.name_scope('dropout'):
+            keep_prob = tf.placeholder(tf.float32)
+            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+        # Map the 1024 features to 10 classes, one for each digit
+        with tf.name_scope('fc2'):
+            W_fc2 = self.weight_variable([1024, 10])
+            b_fc2 = self.bias_variable([10])
+
+            y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        return y_conv, keep_prob
+
+    def save_checkpoint(self):
+        pass
+
+    def restore_checkpoint(self):
+        pass
+
+    def train_batch(self, data, labels):
+        pass
+
+    def test_batch(self, data, labels):
+        pass
+
+    def feed_batch(self, data):
+        pass
+
+    def load_checkpoint(self, dir):
+        pass
+
+    def conv2d(self, x, W):
+        """conv2d returns a 2d convolution layer with full stride."""
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(self, x):
+        """max_pool_2x2 downsamples a feature map by 2X."""
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME')
+
+    def weight_variable(self, shape):
+        """weight_variable generates a weight variable of a given shape."""
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(self, shape):
+        """bias_variable generates a bias variable of a given shape."""
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+
+if __name__ == '__main__':
+    network = Network()
