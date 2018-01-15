0503acb4609f1a4b207bca9e8e9eac8b018a0f69
Rik <Rik@example.com>
2018-01-15 10:12:07 +0100

    alice approach behavior

---
+++ brain/src/behavior/approachtable/approachtable_1.py
@@ -0,0 +1,40 @@
+
+
+import basebehavior.behaviorimplementation
+import rospy
+import actionlib
+
+from alice_msgs.msg import *
+
+class ApproachTable_x(basebehavior.behaviorimplementation.BehaviorImplementation):
+
+
+    def implementation_init(self):
+        
+        self.client = actionlib.SimpleActionClient("aliceapproach", aliceapproachAction);
+        
+        self.state = "connect";
+
+        
+
+    def implementation_update(self):
+
+        if (self.state == "connect"):
+           if (self.client.wait_for_server(rospy.Duration(0.1))):
+               self.state = "send";
+    
+        elif (self.state == "send"):
+            goal = aliceapproachGoal();
+            goal.plane = True;
+            
+            self.client.send_goal(goal);
+            self.state = "wait";
+            
+        elif (self.state == "wait" and self.client.get_state() == actionlib.GoalStatus.ABORTED): # something went wrong
+            result = self.client.get_result();
+            self.set_failed("something went wrong");
+            
+        elif (self.state == "wait" and self.client.get_state() == actionlib.GoalStatus.SUCCEEDED): # 
+            result = self.client.get_result();
+            self.set_finished(); 
+            
