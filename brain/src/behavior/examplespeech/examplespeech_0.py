5e6e1a4fadde465cda84020f55b18412121211fc
Huaitian Lu, Yikun Li <y.li.76@student.rug.nl>
2018-01-19 12:40:35 +0100

    update

--- brain/src/behavior/examplespeech/examplespeech_0.py
+++ brain/src/behavior/examplespeech/examplespeech_0.py
@@ -1,22 +1,22 @@
+import re
+import time
 
 import basebehavior.behaviorimplementation
-import time
-import sys, re
 from JSGFParser import JSGFParser
+
 
 class ExampleSpeech_x(basebehavior.behaviorimplementation.BehaviorImplementation):
 
     def implementation_init(self):
         print "Speech Example Behavior Started!"
-        self.last_recogtime         = time.time()
-        self.state                  = 'idle'
-        self.last_speech_obs        = None  # contains the result and 2best form sphinx
-        self.new_speech_obs         = False # is used for triggering the conversation 
-        
+        self.last_recogtime = time.time()
+        self.state = 'idle'
+        self.last_speech_obs = None  # contains the result and 2best form sphinx
+        self.new_speech_obs = False  # is used for triggering the conversation
+
         self.grammar = JSGFParser('speech/hark-sphinx/grammar/rfai1.gram')
         self.names = self.grammar.findVariableItems("<name>", includeVars=False)
         self.tas = self.grammar.findVariableItems("<ta>", includeVars=False)
-
 
     def update_last_speech_command(self):
 
@@ -32,21 +32,16 @@ class ExampleSpeech_x(basebehavior.behaviorimplementation.BehaviorImplementation
                 self.new_speech_obs = False
 
     def implementation_update(self):
-
         self.update_last_speech_command()
 
         if self.new_speech_obs:
-            
             print self.last_speech_obs
             print self.remove_name(self.last_speech_obs['message'])
             the_ta = self.find_ta(self.last_speech_obs['message'])
             if the_ta:
-                print "so your favorite TA is", the_ta 
+                print "so your favorite TA is", the_ta
 
             self.new_speech_obs = False
-
-
-
 
     ### Helper functions based on JSGFParser ###
 
@@ -54,7 +49,7 @@ class ExampleSpeech_x(basebehavior.behaviorimplementation.BehaviorImplementation
     def remove_name(self, s):
         for name in self.names:
             start = "%s " % name
-            if  s.startswith(start):
+            if s.startswith(start):
                 s = s.lstrip(start)
         return s
 
@@ -73,7 +68,7 @@ class ExampleSpeech_x(basebehavior.behaviorimplementation.BehaviorImplementation
         regex = re.compile("(%s)" % '|'.join(self.tas))
         match = re.split(regex, self.remove_name(string))
 
-        if len(match)<3: return None
+        if len(match) < 3: return None
         return match[1]
 
     # takes as input two hypotheses, s1, s2; and splits both parts
@@ -84,6 +79,6 @@ class ExampleSpeech_x(basebehavior.behaviorimplementation.BehaviorImplementation
         regex = re.compile(" and ")
         matches1 = re.split(regex, s1)
         matches2 = re.split(regex, s2)
-        for x, y in zip(matches1,matches2):
-            splits.append([x,y])
+        for x, y in zip(matches1, matches2):
+            splits.append([x, y])
         return splits
