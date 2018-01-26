09492f02487dad36d3766f94f5b8e34635548420
Huaitian Lu, Yikun Li <y.li.76@student.rug.nl>
2018-01-26 16:43:53 +0100

    Update

--- brain/src/behavior/imageprocess/imageprocess_0.py
+++ brain/src/behavior/imageprocess/imageprocess_0.py
@@ -1,5 +1,3 @@
-
-
 '''
 this is an automatically generated template, if you don't rename it, it will be overwritten!
 '''
@@ -10,28 +8,28 @@ import basebehavior.behaviorimplementation
 class ImageProcess_x(basebehavior.behaviorimplementation.BehaviorImplementation):
 
     def implementation_init(self):
-	self.state = 'start'
+        self.state = 'start'
 
-        #define list of sub-behavior here
+        # define list of sub-behavior here
         self.CreateService()
-        
-        #Init variables
+
+        # Init variables
         self.startService = False
 
         self.selected_behaviors = [
             ("subService", "self.startService == True")
-            ]
+        ]
         pass
 
     def implementation_update(self):
-	if self.state == 'start':
-	    self.CreateService()
-	    self.startService = True
-	    self.state = 'running'
-	elif self.state == 'running' and (self.subService.is_finished() or self.subService.is_failed()):
-	    self.startService = False
-	    self.state = 'start'
-	
+        if self.state == 'start':
+            self.CreateService()
+            self.startService = True
+            self.state = 'running'
+        elif self.state == 'running' and (self.subService.is_finished() or self.subService.is_failed()):
+            self.startService = False
+            self.state = 'start'
+
         pass
 
     def CreateService(self):
--- brain/src/behavior/subimageprocess/subimageprocess_0.py
+++ brain/src/behavior/subimageprocess/subimageprocess_0.py
@@ -1,14 +1,14 @@
-
-
 '''
 this is an automatically generated template, if you don't rename it, it will be overwritten!
 '''
+import random
+
+import actionlib
 import basebehavior.behaviorimplementation
+import rospy
 from part2.msg import *
 from sensor_msgs.msg import Image
-import random
-import actionlib
-import rospy
+
 
 class SubImageProcess_x(basebehavior.behaviorimplementation.BehaviorImplementation):
 
@@ -20,7 +20,7 @@ class SubImageProcess_x(basebehavior.behaviorimplementation.BehaviorImplementati
     def implementation_update(self):
         if self.state == 'running':
             # Subscribe to the kinect2 hd rgb image
-            self.image = rospy.wait_for_message("/kinect2/hd/image_color", Image) 
+            self.image = rospy.wait_for_message("/kinect2/hd/image_color", Image)
             self.min = random.randint(0, 255)
             self.max = random.randint(self.min, 255)
 
@@ -32,26 +32,24 @@ class SubImageProcess_x(basebehavior.behaviorimplementation.BehaviorImplementati
                 self.client.send_goal(rtn)
 
                 self.state = 'waiting'
-		self.now = rospy.get_time()
+                self.now = rospy.get_time()
 
             except Exception as e:
-	        print e
-                print 'Sending goal failed!'
+                print e
+            print 'Sending goal failed!'
 
         elif self.state == 'waiting' and self.client.get_state() == actionlib.GoalStatus.SUCCEEDED:
             result = self.client.get_result()
-	    # print result
+            # print result
 
             if result.sum < 0:
                 self.set_failed("ERROR, Sum < 0")
             else:
-                print 'Amount of the pixes in duration %d-%d is %d.' %(self.min, self.max, result.sum)
-	        self.set_finished()
-	
-	elif self.state == 'waiting' and rospy.get_time() - self.now > 5:
-	    self.client.cancel_all_goals()
-	    self.state = 'running'
-	    print 'Timeout! Restart the request.'
+                print 'Amount of the pixes in duration %d-%d is %d.' % (self.min, self.max, result.sum)
+                self.set_finished()
 
-        pass
+        elif self.state == 'waiting' and rospy.get_time() - self.now > 5:
+            self.client.cancel_all_goals()
+            self.state = 'running'
+            print 'Timeout! Restart the request.'
 
